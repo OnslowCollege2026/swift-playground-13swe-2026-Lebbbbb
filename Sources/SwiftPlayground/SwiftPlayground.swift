998d5cdb0b801220a98321339b4a9c283471ff09
Leb <101553924+Lebbbbb@users.noreply.github.com>
2026-02-10 23:09:45 +0000

    map filter reduce creation

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -2,33 +2,10 @@
 // https://docs.swift.org/swift-book
 // swift run
 
-func totalCost(prices: [Double]) -> Double {
-    var totalCost1 = 0.0
-    for i in prices{
-        totalCost1 += i
-    }
-    return totalCost1
-}
-
-func isOverBudget(total: Double, budget: Double) -> Bool{
-    var overBudget = false
-    
-    return overBudget
-}
 
 @main
 struct SwiftPlayground {
     static func main() {
-        let lunches = [6.50, 8.00, 5.75, 9.20, 7.10]
-        var day = 0
-        //var totalCost = 0.0
-        for i in lunches{
-            //totalCost = totalCost + i
-            day = day + 1
-            print("Day \(day): $\(i)")
-        }
-
-        print ("Total cost: $\(totalCost(prices: lunches))")
-        print ("You are $\(isOverBudget(total: (totalCost(prices: lunches)), budget: 35))")
+        print("Hello, world!")
     }
 }
