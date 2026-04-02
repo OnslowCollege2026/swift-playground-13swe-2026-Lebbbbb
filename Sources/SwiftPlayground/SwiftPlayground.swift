cc41ba1649adf1f619609ef0ad00c1726b4a5a96
Leb <101553924+Lebbbbb@users.noreply.github.com>
2026-04-02 05:56:53 +0000

    grdb work at home, on step 5 of task

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -26,11 +26,38 @@ struct Purchaser: Identifiable, Codable, FetchableRecord, PersistableRecord {
     }
 }
 
+struct Item: Identifiable, Codable, FetchableRecord, PersistableRecord, CustomStringConvertible {
+    let id: Int
+    let name: String
+    let price: Double
+
+    var description: String {
+        "\(id). \(name) - $\(price)"
+    }
+
+    enum CodingKeys: String, CodingKey {
+        case id = "ItemID"
+        case name = "Name"
+        case price = "Price"
+    }
+}
+
+struct Order: Identifiable, Codable, FetchableRecord, PersistableRecord {
+    let id: Int
+    let purchaserID: Int
+    let amount: Double
+
+    enum CodingKeys: String, CodingKey {
+        case id = "OrderID"
+        case purchaserID = "PurchaserID"
+        case amount = "Amount"
+    }
+}
 
 struct OrderLine: Codable, FetchableRecord, PersistableRecord {
     let orderID: Int
-    let itemID: Int
-    let quantity: Int
+    var itemID: Int
+    var quantity: Int
 
     enum Columns {
         static let orderID = Column("OrderID")
@@ -49,18 +76,58 @@ struct SwiftPlayground {
         }
 
         var purchaser: Purchaser? = nil
+        var orderLines: OrderLine? = nil
 
         do {
             try dbQueue.read { db in
                 //try db.dumpSchema()
-                purchaser = try Purchaser.fetchOne(db, key: 1)
-                print(purchaser)
-                purchaser = try Purchaser.fetchOne(db, key: 3)
-                print(purchaser)
+
+                // Fetches two purchasers, handles if nil
+                for i in 2...3 {
+                    purchaser = try Purchaser.fetchOne(db, key: i)
+                    if purchaser == nil {
+                        print("Error: Purchaser not found")
+                    } else {
+                        print(purchaser!)
+                    }
+                }
+                // Prints the details of one item
+                let item = try Item.fetchOne(db, key: 1)
+                print(item!)
             }
         } catch {
-            print("error")
+            print(error)
         }
+
+        do {
+            try dbQueue.read { db in
+                let order = try Order.fetchOne(db, key: 0)
+                // finds the purchaser linked to the order
+                let foundPurchaser = try Purchaser
+                    .filter(Purchaser.Columns.id == order!.purchaserID)
+                    .fetchOne(db)
+                print(foundPurchaser!)
+                
+                orderLines = try OrderLine
+                    .filter(OrderLine.Columns.orderID == order!.id)
+                    .fetchAll(db)
+                orderLines[1].itemID = 2
+                print(orderLines)
+            }
+        } catch {
+            print(error)
+        }
+
+        do {
+            try dbQueue.write { db in
+
+                try orderLines[1].update(db)
+            }
+        } catch {
+            print(error)
+        }
+
+
 
     }
 }
