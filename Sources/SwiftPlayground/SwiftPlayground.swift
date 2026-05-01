661718235a488f3191c12012b6b7f6fa8b29e91e
Leb <101553924+Lebbbbb@users.noreply.github.com>
2026-05-01 00:59:00 +0000

    Fri May1 class

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -10,6 +10,8 @@ TODO!
 - Orders
 
 - put in hard data, make a toggle in code to enable/disable
+
+round(value * 100) / 100.0
 */
 
 
@@ -17,14 +19,26 @@ TODO!
 struct Instrument {
     let id: Int
     var type: String
-    let price: Double
+    var price: Double
     var totalStock: Int
     var rented: Int
     var broken: Int
 
-    func instrumentSummary() -> String {
+    /// calculates the total stock including broken, minus rented
+    var totalNotRented: Int {
+        return totalStock - rented
+    }
+
+    func instrumentSummary(totalOrAval: String) -> String {
         //return("\(id+1). A \(type) costs $\(price) per day. We have \(totalStock - rented - broken) avalable, \(rented) rented & \(broken) broken units")
-        return("\(id+1). \(type) costs $\(price) per day. \(totalStock - rented - broken) avalable, \(rented) rented & \(broken) broken units")
+        if totalOrAval == "avalable" {
+            return("\(id+1). \(type) costs $\(price) per day. \(totalStock - rented - broken) avalable, \(rented) rented & \(broken) broken units")
+        } else if totalOrAval == "total" {
+            return("\(id+1). \(type) costs $\(price) per day. \(totalStock) total, \(rented) rented & \(broken) broken units")
+        } else {
+            print("CODE ERROR. instrumentSummary() passed incorrect information.")
+            return("")
+        }
     }
 }
 
@@ -48,13 +62,18 @@ struct Order {
 @main
 struct SwiftPlayground {
     static func main() {
+        let maxName = 40
         var userText = ""
+        var selIndex = ""
+        //var counter = 0
+        /// misspelt as continue is a default var
+        var contin = false
 
         var instruments = [
             Instrument(id: 0, type: "Acoustic Guitar", price: 10.0, totalStock: 20, rented: 4, broken: 1),
             Instrument(id: 1, type: "Electric Guitar", price: 25, totalStock: 6, rented: 1, broken: 0)]
 
-
+        let instProp = ["id", "name", "price", "total stock", "rented", "broken"]
 
 
         mainMenu()
@@ -108,29 +127,134 @@ struct SwiftPlayground {
         func stockSummary() {
             print("\nStock summary.")
             for item in instruments {
-                print((item.instrumentSummary()))
+                print((item.instrumentSummary(totalOrAval: "avalable")))
             }
             print()
         }
 
         func modifyInstrument() {
+            print("\nNote: The total rented cannot be edited though this.")
             print("Please type the number for the instrument you want to modify")
             for item in instruments {
-                print((item.instrumentSummary()))
+                print((item.instrumentSummary(totalOrAval: "total")))
             }
-            userText = readLine()!
-            if Int(userText)! > 0 && Int(userText)! < (instruments.count + 1) {
-                print ("VALID")
+            selIndex = readLine()!
+            if typeCheck(inputSTR: selIndex, type: "Int") == true {
+                if Int(selIndex)! > 0 && Int(selIndex)! < (instruments.count + 1) {
+                    contin = false
+                    while contin == false {
+                        print ("\nPlease enter a new name. Leave blank to leave it as \(instruments[Int(selIndex)! - 1].type).")
+                        userText = readLine()!
+                        if userText == "" {
+                            contin = true
+                        } else if userText.count > maxName {
+                            print("Name too long. Must be under \(maxName + 1) characters.")
+                        }
+                        else {
+                            instruments[Int(selIndex)! - 1].type = userText
+                            contin = true
+                        }
+                    }
+
+                    //print("DEBUG! \(instruments[Int(selIndex)! - 1].type)") // DEBUG!!
+                    contin = false
+                    while contin == false {
+                        print ("\nPlease enter a new price. Leave blank to leave it as $\(instruments[Int(selIndex)! - 1].price).")
+                        userText = readLine()!
+                        if userText == "" {
+                            contin = true
+                        } else if typeCheck(inputSTR: userText, type: "Double") == false {
+                            print ("Please enter a whole or decimal number above 0.")
+                        } else if typeCheck(inputSTR: userText, type: "Double") == true {
+                            if Double(userText)! > 0 {
+                                instruments[Int(selIndex)! - 1].price = Double(userText)!
+                                contin = true
+                            } else {
+                                print ("Please enter a whole or decimal number above 0.")
+                            }
+                        }
+                        /*else {
+                            instruments[Int(selIndex)! - 1].price = Double(userText)!
+                            contin = true
+                        }*/
+                    }
+
+                    print("DEBUG! \(instruments[Int(selIndex)! - 1].price)")
+                    contin = false
+                    while contin == false {
+                        print ("\nPlease enter a new total stock amount. Leave blank to leave it as \(instruments[Int(selIndex)! - 1].totalStock).")
+                        userText = readLine()!
+                        if userText == "" {
+                            contin = true
+                        } else if typeCheck(inputSTR: userText, type: "Int") == false {
+                            print ("Please enter a whole number above 0.")
+                        } else if typeCheck(inputSTR: userText, type: "Int") == true {
+                            if Int(userText)! >= 0 {
+                                instruments[Int(selIndex)! - 1].totalStock = Int(userText)!
+                                contin = true
+                            } else {
+                                print ("Please enter a whole number above 0.")
+                            }
+                        }
+                    }
+
+                    contin = false
+                    while contin == false {
+                        print ("\nPlease enter a new amount of broken units. Leave blank to leave it as \(instruments[Int(selIndex)! - 1].broken).")
+                        userText = readLine()!
+                        if userText == "" {
+                            contin = true
+                        } else if typeCheck(inputSTR: userText, type: "Int") == false {
+                            print ("Please enter a whole number above 0.")
+                        } else if typeCheck(inputSTR: userText, type: "Int") == true {
+                            if Int(userText)! >= 0 {
+                                if Int(userText)! > (instruments[Int(selIndex)!].totalStock - instruments[Int(selIndex)!].rented) {
+                                    print ("Cannot enter number higher than total avalable stock (\(instruments[Int(selIndex)!].totalNotRented))")
+                                } else {
+                                    instruments[Int(selIndex)! - 1].broken = Int(userText)!
+                                    contin = true
+                                }
+                            } else if Int(userText)! < 0 {
+                                print ("Please enter a whole number above 0.")
+                            }
+                        }
+                    }
+                } else {
+                    print("Please enter a valid entry.")
+                }
             } else {
-                print("INVALID")
+                print("Please enter a valid entry.")
+            }
+        }
+
+
+// this is just an idea to see if making this into 1 function is cleaner
+// !!!!!!!!! how to use string as input in place of .type
+        func instEdit(action: String, instIndex: Int, cell: String, inputType: String) {
+            if action == "Modify" {
+                //print("Please enter the new  for \(instruments[instIndex].cell)")
             }
         }
 
 
 
-        func intChecker(inputSTR: String) -> Bool{
-            if let Int(inputSTR) {
-
+        func typeCheck(inputSTR: String, type: String) -> Bool{
+            if type == "Int" {
+                if let _ = Int(inputSTR) {
+                    return true
+                } else {
+                    return false
+                }
+            }
+            if type == "Double" {
+                if let _ = Double(inputSTR) {
+                    return true
+                } else {
+                    return false
+                }
+            } else {
+                print("CODE ERROR. Incorrect type supplied to typeCheck()")
+                return false
             }
         }
 
