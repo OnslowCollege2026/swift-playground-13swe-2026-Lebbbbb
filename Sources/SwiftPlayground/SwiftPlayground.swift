693ac34e8afec156d04824759a81f81627cb8bc0
Leb <101553924+Lebbbbb@users.noreply.github.com>
2026-05-06 00:58:57 +0000

    family code

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -12,17 +12,19 @@ TODO!
 - put in hard data, make a toggle in code to enable/disable
 
 round(value * 100) / 100.0
+catagory for amps and stuff and ask user if they want an amp if theyre hiring an eletric gutair
 */
 
 
 /// Instrument type and stock
 struct Instrument {
     // i keep id for list printing. Its simpler to me
-    let id: Int
-    var type: String
+    var id: Int
+    var name: String
     var totalStock: Int
     var rented: Int
     var broken: Int
+    var family: Int
 
     /// calculates the total stock including broken, minus rented
     var totalNotRented: Int {
@@ -32,13 +34,14 @@ struct Instrument {
     var totalUnavalable: Int {
         return rented + broken
     }
+    
 
     func instrumentSummary(totalOrAval: String) -> String {
         //return("\(id+1). A \(type) costs $\(price) per day. We have \(totalStock - rented - broken) avalable, \(rented) rented & \(broken) broken units")
         if totalOrAval == "avalable" {
-            return("\(id+1). \(type) has \(totalStock - rented - broken) avalable currently, \(rented) rented & \(broken) broken units")
+            return("\(id+1). \(name) has \(totalStock - rented - broken) avalable currently, \(rented) rented,  \(broken) broken units & is a \(instType[family])")
         } else if totalOrAval == "total" {
-            return("\(id+1). \(type) has \(totalStock) total, \(rented) rented & \(broken) broken units")
+            return("\(id+1). \(name) has \(totalStock) total, \(rented) rented & \(broken) broken units")
         } else {
             print("CODE ERROR. instrumentSummary() passed incorrect information.")
             return("")
@@ -68,18 +71,21 @@ struct SwiftPlayground {
     static func main() {
         let maxName = 40
         var userText = ""
-        var selIndex = ""
+        var selIndex = -1
         //var counter = 0
         /// misspelt as continue is a default var
         var contin = false
 
+        var instType = ["Plucked string", "Bowed string", "Piano", "Brass", "Woodwind", "Percussion"]
+
         var instruments = [
-            Instrument(id: 0, type: "Acoustic Guitar", totalStock: 20, rented: 4, broken: 1),
-            Instrument(id: 1, type: "Electric Guitar", totalStock: 6, rented: 1, broken: 0)]
+            Instrument(id: 0, name: "Acoustic Guitar", totalStock: 20, rented: 4, broken: 1, family: 1),
+            Instrument(id: 1, name: "Electric Guitar", totalStock: 6, rented: 1, broken: 0, family: 1)]
 
-        var tempList: [Any] = [-1, "TEMP", -1, -1, -1]
-
-        //let instProp = ["id", "name", "price", "total stock", "rented", "broken"]
+        //var tempInst: [Any] = [-1, "TEMP", -1, -1, -1, -1]
+        var tempInst = Instrument(id: -1, name: "TEMP", totalStock: -1, rented: -1, broken: -1, family: -1)
+        // used to reset back to default
+        let instDefault = Instrument(id: -1, name: "TEMP", totalStock: -1, rented: -1, broken: -1, family: -1)
 
 
         mainMenu()
@@ -144,38 +150,47 @@ struct SwiftPlayground {
             for item in instruments {
                 print((item.instrumentSummary(totalOrAval: "total")))
             }
-            selIndex = readLine()!
-            if typeCheck(inputSTR: selIndex, type: "Int") == true {
-                if Int(selIndex)! > 0 && Int(selIndex)! < (instruments.count + 1) {
+            //selIndex = readLine()!
+            userText = readLine()!
+            if typeCheck(inputSTR: userText, type: "Int") == true {
+                if Int(userText)! > 0 && Int(userText)! < (instruments.count + 1) {
+                    selIndex = Int(userText)! - 1
+                    tempInst = instDefault
+                    tempInst.id = selIndex
+                    tempInst.rented = instruments[selIndex].rented
+
                     contin = false
                     while contin == false {
-                        print ("\nPlease enter a new name. Leave blank to leave it as \(instruments[Int(selIndex)! - 1].type).")
+                        print ("\nPlease enter a new name. Leave blank to leave it as \(instruments[selIndex].name).")
                         userText = readLine()!
                         if userText == "" {
+                            tempInst.name = instruments[selIndex].name
                             contin = true
                         } else if userText.count > maxName {
                             print("Name too long. Must be under \(maxName + 1) characters.")
-                        }
-                        else {
-                            instruments[Int(selIndex)! - 1].type = userText
+                        } else {
+                            tempInst.name = userText
+                            //instruments[selIndex].name = userText
                             contin = true
                         }
                     }
 
                     contin = false
                     while contin == false {
-                        print ("\nPlease enter a new amount of broken units. Leave blank to leave it as \(instruments[Int(selIndex)! - 1].broken).")
+                        print ("\nPlease enter a new amount of broken units. Leave blank to leave it as \(instruments[selIndex].broken).")
                         userText = readLine()!
                         if userText == "" {
+                            tempInst.broken = instruments[selIndex].broken
                             contin = true
                         } else if typeCheck(inputSTR: userText, type: "Int") == false {
                             print ("Please enter a whole positive number.")
                         } else if typeCheck(inputSTR: userText, type: "Int") == true {
                             if Int(userText)! >= 0 {
-                                if Int(userText)! > (instruments[Int(selIndex)! - 1].totalStock - instruments[Int(selIndex)! - 1].rented) {
-                                    print ("Cannot enter number higher than total avalable stock (\(instruments[Int(selIndex)! - 1].totalNotRented))")
+                                if Int(userText)! > (instruments[selIndex].totalStock - instruments[selIndex].rented) {
+                                    print ("Cannot enter number higher than total avalable stock (\(instruments[selIndex].totalNotRented))")
                                 } else {
-                                    instruments[Int(selIndex)! - 1].broken = Int(userText)!
+                                    tempInst.broken = Int(userText)!
+                                    //instruments[selIndex].broken = Int(userText)!
                                     contin = true
                                 }
                             } else if Int(userText)! < 0 {
@@ -186,18 +201,41 @@ struct SwiftPlayground {
 
                     contin = false
                     while contin == false {
-                        print ("\nPlease enter a new total stock amount. Leave blank to leave it as \(instruments[Int(selIndex)! - 1].totalStock).")
+                        print ("\nPlease enter a new total stock amount. Leave blank to leave it as \(instruments[selIndex].totalStock).")
                         userText = readLine()!
                         if userText == "" {
+                            tempInst.totalStock = instruments[selIndex].totalStock
                             contin = true
                         } else if typeCheck(inputSTR: userText, type: "Int") == false {
                             print ("Please enter a whole positive number.")
                         } else if typeCheck(inputSTR: userText, type: "Int") == true {
-                            if Int(userText)! - instruments[Int(selIndex)! - 1].totalUnavalable >= 0 {
-                                instruments[Int(selIndex)! - 1].totalStock = Int(userText)!
+                            if Int(userText)! - instruments[selIndex].totalUnavalable >= 0 {
+                                tempInst.totalStock = Int(userText)!
+                                //instruments[selIndex].totalStock = Int(userText)!
                                 contin = true
                             } else {
-                                print("New stock cannot be below 0 or total unavalable units. (\(instruments[Int(selIndex)! - 1].totalUnavalable))")
+                                print("New stock cannot be below 0 or total unavalable units. (\(instruments[selIndex].totalUnavalable))")
+                            }
+                        }
+                    }
+
+                    contin = false
+                    while contin == false {
+                        print("\nPlease enter a new family type. Leave blank to leave it as \(instruments[selIndex].family).")
+                        listFamilies()
+                        //print("Leave blank to leave as \(instruments[selIndex].family).\nNew family:", terminator: " ")
+                        userText = readLine()!
+                        if userText == "" {
+                            tempInst.family = instruments[selIndex].family
+                            contin = true
+                        } else if typeCheck(inputSTR: userText, type: "Int") == false {
+                            print ("Please enter a whole positive number.")
+                        } else if typeCheck(inputSTR: userText, type: "Int") == true {
+                            if Int(userText)! <= 0 || Int(userText)! > instType.count {
+                                print ("Please enter a valid entry.")
+                            } else {
+                                tempInst.family = Int(userText)!
+                                contin = true
                             }
                         }
                     }
@@ -207,6 +245,22 @@ struct SwiftPlayground {
                 }
             } else {
                 print("Please enter a valid entry.")
+            }
+
+            contin = false
+            print("\n\(tempInst.instrumentSummary(totalOrAval: "total"))")
+            print("Confirm your changes? Y/n")
+            while contin == false {
+                userText = readLine()!
+                if userText.lowercased() == "y" || userText == "" {
+                    print("change")
+                    contin = true
+                } else if userText.lowercased() == "n" {
+                    print("discard")
+                    contin = true
+                } else {
+                    print("Please enter Y/n")
+                }
             }
             //print("DEBUG \(instruments[0].instrumentSummary(totalOrAval: "avalable"))")
         }
@@ -221,7 +275,7 @@ struct SwiftPlayground {
                 if userText.count == 0 || userText.count > maxName {
                     print("Name must be between 0 and \(maxName + 1) characters\n")
                 } else {
-                    tempList[1] = userText
+                    tempInst.name = userText
                     contin = true
                 }
             }
@@ -231,7 +285,7 @@ struct SwiftPlayground {
                 userText = readLine()!
                 if typeCheck(inputSTR: userText, type: "Int") == true {
                     if Int(userText)! >= 0 {
-                        tempList[2] = userText
+                        tempInst.totalStock = Int(userText)!
                     } else {
                         print("Stock must be a positive whole number")
                     }
@@ -239,7 +293,7 @@ struct SwiftPlayground {
                     print("Stock must be a positive whole number")
                 }
             }
-            print(tempList)
+            print(tempInst)
 
             //make confirm screen beofre it saves, ask if user is sure they want one with 0 stock (if 0)
         }
@@ -262,6 +316,12 @@ struct SwiftPlayground {
             } else {
                 print("CODE ERROR. Incorrect type supplied to typeCheck()")
                 return false
+            }
+        }
+
+        func listFamilies() {
+            for (index, item) in instType.enumerated() {
+                print("\(index + 1). \(item)")
             }
         }
 
